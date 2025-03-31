3f96792941e163f8c573b64191aba225cbb6d492
Kyungsu Kim <roomformilk683@gmail.com>
2025-03-31 23:44:00 +0100

    Add Docker deployment pipeline and integration container setup

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
 
-# Update system packages and install Node.js and npm
-sudo apt update && sudo apt install -y nodejs npm
+# Check if there is an instance running with the image name we are deploying
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 
-# Install pm2, which is a process manager for Node.js applications
-sudo npm install -g pm2
+# If an instance does exist, stop the instance
+if [ "$CURRENT_INSTANCE" ]
+then
+  docker rm $(docker stop $CURRENT_INSTANCE)
+fi
 
-# Stop any currently running instance of the application (ignore errors if it doesn't exist)
-pm2 stop book_app
+# Pull down the instance from Docker Hub
+docker pull $IMAGE_NAME
 
-# Navigate to the application directory
-cd BookApp/
+# Check if a docker container exists with the name of node_app, if it does remove the container
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ]
+then
+  docker rm node_app
+fi
 
-# Install application dependencies
-npm install
+# Create a container called node_app that is available on port 8443 from our docker image
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
+
+# Write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
+
+# Write the server key to a file
 echo $SERVER > server.crt
 
-# Start the application using pm2 with the process name "ebook_app"
-pm2 start ./bin/www --name book_app
+# Add the private key to the node_app docker container
+docker cp ./privatekey.pem node_app:/privatekey.pem
+
+# Add the server key to the node_app docker container
+docker cp ./server.crt node_app:/server.crt
+
+# Start the node_app container
+docker start node_app
